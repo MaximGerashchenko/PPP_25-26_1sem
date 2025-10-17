f5b325aedef0f929f9a39c92881ac83ef3ddd8fc
MaximGerashchenko <7606122@mail.ru>
2025-10-17 14:23:56 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,25 @@
-
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+s = input()
+st = []
+for i in range(len(s)):
+    c = s[i]
+    if c in '([{':
+        st.append(c)
+    elif c == ')':
+        if not st or st[-1] != '(':
+            print(i + 1)
+            exit()
+        st.pop()
+    elif c == ']':
+        if not st or st[-1] != '[':
+            print(i + 1)
+            exit()
+        st.pop()
+    elif c == '}':
+        if not st or st[-1] != '{':
+            print(i + 1)
+            exit()
+        st.pop()
+if st:
+    print(len(s) + 1)
+else:
+    print('ok')
